712118e3167222ab59858954ad17b11d737ac397
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-09 18:53:22 +0330

    Complete run method of BlackThread

--- src/main/java/sbu/cs/PrioritySimulator/BlackThread.java
+++ src/main/java/sbu/cs/PrioritySimulator/BlackThread.java
@@ -23,6 +23,7 @@ public class BlackThread extends ColorThread {
 
     @Override
     public void run() {
-        // TODO call printMessage
+        printMessage();
+        CDL.countDown();
     }
 }
