a579554b58139c628cffc344ad8c777405cae056
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-10 09:56:03 +0330

    Add TermCalculator class to calculate each term

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,5 +1,12 @@
 package sbu.cs.CalculatePi;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class PiCalculator {
 
     /**
@@ -14,6 +21,42 @@ public class PiCalculator {
      * @param floatingPoint the exact number of digits after the floating point
      * @return pi in string format (the string representation of the BigDecimal object)
      */
+
+    public static BigDecimal pi = new BigDecimal(0);
+
+    public static class TermCalculator implements Runnable {
+        MathContext mc = new MathContext(1000);
+        BigDecimal numerator = new BigDecimal(4);
+        int floatingPoint;
+        int n; // number of the term
+
+        public TermCalculator (int n, int floatingPoint) {
+            this.n = n;
+            this.floatingPoint = floatingPoint;
+        }
+
+        public void run(){
+            BigDecimal sign = new BigDecimal(1);
+            if (n%2 == 1) {
+                sign = new BigDecimal(-1);
+            }
+
+            BigDecimal term = new BigDecimal(1);
+            term = term.multiply(sign, mc);
+            term = term.multiply(numerator, mc);
+            term = term.divide(denominator(n), mc);
+            addTerm(term, mc);
+        }
+
+        private BigDecimal denominator (int n){
+            BigDecimal den = new BigDecimal(Integer.toString(2*n + 1));
+            return den;
+        }
+    }
+
+    public static synchronized void addTerm(BigDecimal term, MathContext mc){
+        pi = pi.add(term, mc);
+    }
 
     public String calculate(int floatingPoint)
     {
