b0ab8e393f6ebe37b9ccee20d5fef0dec1534051
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-10 09:49:47 +0330

    handle access of two threads with semaphore

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -14,7 +14,20 @@ public class Operator extends Thread {
     public void run() {
         for (int i = 0; i < 10; i++)
         {
-            Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+            try {
+                System.out.println(Thread.currentThread().getName() + " is waiting for a permit.");
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + " gets a permit.");
+                Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+                // printing any phrase here will not help to see releasing semaphore before acquiring it.
+                // cause immediately after release, it will be acquired and printing any statement looks like the first
+                // event was acquiring before releasing!
+            }
+
             try {
                 sleep(500);
             } catch (InterruptedException e) {
