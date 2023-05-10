1c5a0bc71e8ba1915fa422e251a1ecdc90a2b19e
MaedehBDNF <badanfirouzm@gmail.com>
2023-05-10 09:45:17 +0330

    Add statement for notify that thread accessed the resource

--- src/main/java/sbu/cs/Semaphore/Resource.java
+++ src/main/java/sbu/cs/Semaphore/Resource.java
@@ -1,10 +1,17 @@
 package sbu.cs.Semaphore;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Resource {
 
     public static void accessResource() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+
         try {
-            Thread.sleep(100);
+            System.out.println(Thread.currentThread().getName() + " accessed to the resource at " + formatter.format(date));
+             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
